6289cc0d65e9abe3610316db15c68c9e48b6654f
Chung_es <44948482+euisuk-chung@users.noreply.github.com>
2024-05-03 15:27:16 +0900

    Create remove_nvidia.sh

---
+++ scripts/remove_nvidia.sh
@@ -0,0 +1,6 @@
+# Shell script
+sudo apt-get update -y && sudo apt-get upgrade -y
+sudo apt-get purge -y nvidia*
+sudo apt-get autoremove
+sudo apt-get autoclean
+sudo rm -rf /usr/local/cuda*$(uname -r)
