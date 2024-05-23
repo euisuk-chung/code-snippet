0ae26671f1da9571936154eafa509285947053f1
Chung_es <44948482+euisuk-chung@users.noreply.github.com>
2024-05-23 16:38:21 +0900

    Update install_pyenv.sh

--- scripts/install_pyenv.sh
+++ scripts/install_pyenv.sh
@@ -17,7 +17,8 @@ sudo apt-get update
 
 sudo apt-get install -y make build-essential libssl-dev zlib1g-dev \
 libbz2-dev libreadline-dev libsqlite3-dev wget curl llvm \
-libncurses5-dev libncursesw5-dev xz-utils tk-dev libffi-dev liblzma-dev python-openssl git
+libncurses5-dev libncursesw5-dev xz-utils tk-dev libffi-dev liblzma-dev \
+python3-openssl git
  
 # pyenv 설치
 if [ ! -d "${HOME}/.pyenv" ]; then
